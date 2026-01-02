8123d72b6b3cb41a96667b8d792ec3c834f7807d
Rezapu Obed <obed.ubaidillah@gmail.com>
2026-01-02 18:01:59 +0700

    fix: use mysql adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const url = new URL(process.env.DATABASE_URL!.replace('mysql://', 'http://'));
+
+        const adapter = new PrismaMariaDb({
+            host: url.hostname,
+            port: parseInt(url.port) || 3306,
+            user: url.username,
+            password: url.password,
+            database: url.pathname.slice(1),
+            connectionLimit: 10,
+        });
+
+        super({ adapter });
+    }
+
     async onModuleInit(): Promise<void> {
         await this.$connect();
     }
@@ -11,3 +27,4 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
         await this.$disconnect();
     }
 }
+
